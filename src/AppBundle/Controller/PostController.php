aedd617b69ff6061eb966bd8aa5846ce8127dded
geoffreyhale <geoffreyhale@gmail.com>
2016-08-14 17:28:09 -0700

    add $slug to post_show route

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -9,13 +9,13 @@ use Symfony\Component\HttpFoundation\Response;
 class PostController extends Controller
 {
     /**
-     * @Route("/post", name="post")
+     * @Route("/post/{slug}", name="post_show")
      */
-    public function indexAction()
+    public function showAction($slug)
     {
-        return $this->render('post/index.html.twig', array(
-            'title' => 'My First Post Title',
-            'body' => 'My first post body.'
+        return $this->render('post.html.twig', array(
+            'title' => 'This is the PostController showAction page for $slug = "' . $slug . '".',
+            'body' => 'This is a post body for $slug = "' . $slug . '".'
         ));
     }
 }
