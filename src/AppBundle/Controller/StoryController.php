5cfe4d170e3dae9fa34a88e1d52c604b3a19c319
geoffreyhale <geoffreyhale@gmail.com>
2016-08-21 17:04:12 -0700

    add create story route

--- src/AppBundle/Controller/StoryController.php
+++ src/AppBundle/Controller/StoryController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Story;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,5 +19,21 @@ class StoryController extends Controller
             'body' => 'Body for Story #' . $slug,
             'id' => $slug
         ));
+    }
+
+    /**
+     * @Route("/story/create/{title}/{body}", name="story_create")
+     */
+    public function createAction($title, $body)
+    {
+        $story = new Story();
+        $story->setTitle($title);
+        $story->setBody($body);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($story);
+        $em->flush();
+
+        return new Response('Created new story with id ' . $story->getId());
     }
 }
