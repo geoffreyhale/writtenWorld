66096ee64fe0f6d9bced0d2021fa19acbf9ee1a0
geoffreyhale <geoffreyhale@gmail.com>
2016-08-27 19:23:35 -0700

    add create/edit location

--- src/AppBundle/Controller/LocationController.php
+++ src/AppBundle/Controller/LocationController.php
@@ -60,4 +60,82 @@ class LocationController extends Controller
             'locations' => $locations
         ));
     }
+
+    /**
+     * @Route("/location/create/", name="location_create")
+     */
+    public function createAction(Request $request)
+    {
+        $location = new Location();
+
+        $form = $this->createFormBuilder($location)
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $location = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($location);
+            $em->flush();
+
+            return $this->redirectToRoute('location_edit', array (
+                'locationId' => $location->getId()
+            ));
+        }
+
+        return $this->render('location/create.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * @Route("/location/edit/{locationId}", name="location_edit")
+     */
+    public function editAction($locationId, Request $request)
+    {
+        $location = new Location();
+
+        if ($locationId) {
+            /** @var Location $location */
+            $location = $this->getDoctrine()
+                ->getRepository('AppBundle:Location')
+                ->find($locationId);
+
+            if (!$location) {
+                throw $this->createNotFoundException(
+                    'No location found for id ' . $locationId
+                );
+            }
+        }
+
+        $form = $this->createFormBuilder($location)
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('save', SubmitType::class, array('label' => 'Save'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $location = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($location);
+            $em->flush();
+
+            return $this->redirectToRoute('location_edit', array (
+                'locationId' => $location->getId()
+            ));
+        }
+
+        return $this->render('location/edit.html.twig', array(
+            'form' => $form->createView(),
+            'id' => $location->getId(),
+        ));
+    }
 }
